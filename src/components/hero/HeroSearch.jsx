62568ded0e87cd304c0c01f028f741d8aacb85f9
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-07-12 03:53:40 -0700

    hero section

--- src/components/hero/HeroSearch.jsx
+++ src/components/hero/HeroSearch.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./HeroSearch.module.css";
 
 const HeroSearch = () => {
-  return <div>hello</div>;
+  return (
+    <div className={styles["hero__search-container"]}>
+      <form className={styles["search__form"]}>
+        <label htmlFor="search">FIND YOUR FAVORITE MEALS</label>
+        <div className={styles["search__input-container"]}>
+          <input id="search" type="text" />
+          <button>Search</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default HeroSearch;
