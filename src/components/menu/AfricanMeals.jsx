a9a253763f446c92c1702d99c563655767b92185
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-07-13 07:51:19 -0700

    fetching meals

--- src/components/menu/AfricanMeals.jsx
+++ src/components/menu/AfricanMeals.jsx
@@ -1,76 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AfricanMeals.module.css";
-import img from "../../assets/background-image-1.jpg";
-
-const mealsList = [
-  {
-    id: 1,
-    name: "First African Meal",
-    price: 23445,
-  },
-  {
-    id: 2,
-    name: "First African Meal",
-    price: 23445,
-  },
-  {
-    id: 3,
-    name: "First African Meal",
-    price: 23445,
-  },
-  {
-    id: 4,
-    name: "First African Meal",
-    price: 23445,
-  },
-  {
-    id: 5,
-    name: "First African Meal",
-    price: 23445,
-  },
-  {
-    id: 6,
-    name: "First African Meal",
-    price: 23445,
-  },
-  {
-    id: 7,
-    name: "First African Meal",
-    price: 23445,
-  },
-  {
-    id: 8,
-    name: "First African Meal",
-    price: 23445,
-  },
-];
 
 const AfricanMeals = () => {
+  const [meals, setMeals] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://foodease-backend-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      const foodData = await response.json();
+
+      const loadedMeals = [];
+      for (const keys in foodData) {
+        loadedMeals.push({
+          id: keys,
+          image: foodData[keys].image,
+          name: foodData[keys].name,
+          price: foodData[keys].price,
+          discount: foodData[keys].price - (foodData[keys].price * 10) / 100,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  if (!meals) {
+    return (
+      <section /*className={styles.loadingState}*/>
+        <p>LOADING...</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
+    <li key={meal.id} className={styles.meal}>
+      <article className={styles["meal__article"]}>
+        <img src={meal.image} alt="meal" />
+
+        {/* meals title container */}
+        <div className={styles["meals__title-container"]}>
+          <p className={styles["meal__title"]}>{meal.name}</p>
+
+          {/* meals price container */}
+          <div className={styles["meal__price-container"]}>
+            <p className={styles["meal__price"]}>N{meal.discount}</p>
+            <p className={styles["meal__oiginal-price"]}>N{meal.price}</p>
+          </div>
+        </div>
+      </article>
+    </li>
+  ));
+
   return (
     <div className={styles["african__meals-container"]}>
       <h3>African Meals</h3>
       <div className={styles["african__meals"]}>
-        <ul className={styles["meals__list"]}>
-          {/* meals lists */}
-          {mealsList.map((meals) => (
-            <li key={meals.id} className={styles.meal}>
-              <article className={styles["meal__article"]}>
-                <img src={img} alt="meal" />
-
-                {/* meals title container */}
-                <div className={styles["meals__title-container"]}>
-                  <p className={styles["meal__title"]}>First African Meal</p>
-
-                  {/* meals price container */}
-                  <div className={styles["meal__price-container"]}>
-                    <p className={styles["meal__price"]}>N3344</p>
-                    <p className={styles["meal__oiginal-price"]}>N1234</p>
-                  </div>
-                </div>
-              </article>
-            </li>
-          ))}
-        </ul>
+        {meals && <ul className={styles["meals__list"]}>{mealsList}</ul>}
       </div>
     </div>
   );
