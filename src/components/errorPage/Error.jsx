51145369a0da306a5418513560e95ef8f6b8151d
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-07-30 15:44:30 -0700

    error page

--- src/components/errorPage/Error.jsx
+++ src/components/errorPage/Error.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 // importing styles
 import styles from "./Error.module.css";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <section className={styles.section}>Error</section>;
+  return (
+    <section className={styles.section}>
+      <h1 className={styles["section__header"]}>Error</h1>
+      <p>Page not found</p>
+      <Link to="/" style={{ color: "black" }}>
+        Back to Home
+      </Link>
+    </section>
+  );
 };
 
 export default Error;
