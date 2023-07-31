7bc676d0ebf9a46db806099b75a6acab3ee4cbab
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-07-31 04:28:04 -0700

    error page

--- src/components/errorPage/Error.jsx
+++ src/components/errorPage/Error.jsx
@@ -9,7 +9,17 @@ const Error = () => {
     <section className={styles.section}>
       <h1 className={styles["section__header"]}>Error</h1>
       <p>Page not found</p>
-      <Link to="/" style={{ color: "black" }}>
+      <Link
+        to="/"
+        style={{
+          color: "black",
+          backgroundColor: "var(--color-secondary)",
+          padding: "0.5rem 1rem",
+          width: "10rem",
+          margin: "2rem auto",
+          borderRadius: "1rem",
+        }}
+      >
         Back to Home
       </Link>
     </section>
