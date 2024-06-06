b2e5db16c4d6b8f5d2817a58a83ab82cc6900188
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2024-06-06 21:46:46 +0100

    test

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import About from "./components/about/About";
 import ErrorPage from "./pages/ErrorPage";
 
 // adding routes
+// adding routes
 const router = createBrowserRouter([
   {
     path: "/",
